04327b31591b188f3448b15fdc059042d0b95b11
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-03-30 14:10:23 +0530

    Add files via upload

--- Stress/flask-app.py
+++ Stress/flask-app.py
@@ -1,53 +1,80 @@
+# 1. Import the dependencies
 from flask import Flask, request
-from flask_restx import Api, Resource, fields
 import pandas as pd
+import numpy as np
 import joblib
 import os
+import flasgger
+from flasgger import Swagger, swag_from
 
+# 2. Initialize the Flask app and Swagger UI
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Stress Prediction API', description='API to predict stress labels')
+swagger = Swagger(app)
 
-# Loading the trained models
-nb_model = joblib.load('MultinomialNB_stress.pkl')
+# 3. Read the pickle file
 bb_model = joblib.load('BernoulliNB_stress.pkl')
 
-# Swagger UI input model for POST request
-text_input_model = api.model('Text_Input', {
-    'text': fields.String(description='Text for stress prediction', example='I am feeling stressed')
-})
+# 4. Endpoint for GET request
+@app.route('/predict_text', methods=["GET"])
+#@swag_from('text_predict.yml')
+def predict_text():
+    """
+    Endpoint to predict stress labels from text input.
+    ---
+    parameters:
+        - name: text
+          in: query
+          type: string
+          required: true
+          description: Text for stress prediction
+          example: "I am feeling stressed"
+    responses:
+        200:
+            description: Predicted stress label
+            schema:
+                type: object
+                properties:
+                    label:
+                        type: integer
+                        description: Predicted stress label (0 or 1)
+                        example: 1
+    """
+    text = request.args.get('text')
+    label = bb_model.predict([text])[0]
+    return {'label': int(label)}
 
-# Swagger UI input model for GET request
-file_input_model = api.model('File_Input', {
-    'file': fields.String(description='CSV file containing texts for stress prediction with header "text"', 
-                           example='file.csv')
-})
-
-# Swagger UI output model
-text_output_model = api.model('Text_Output', {
-    'label': fields.Integer(description='Predicted stress label (0 or 1)', example=1)
-})
-
-# Endpoint for POST request
-@api.route('/predict_text')
-class Text_Predict(Resource):
-    @api.expect(text_input_model)
-    @api.marshal_with(text_output_model)
-    def post(self):
-        text = request.json['text']
-        label = nb_model.predict([text])[0]
-        return {'label': int(label)}
-
-# Endpoint for GET request
-@api.route('/predict_file')
-class File_Predict(Resource):
-    @api.expect(file_input_model)
-    def get(self):
-        file_path = request.args.get('file')
-        df = pd.read_csv(file_path)
-        texts = df['text'].tolist()
-        labels = nb_model.predict(texts)
-        df['label'] = labels
-        return df.to_dict()
+# 5. Endpoint for POST request
+@app.route('/predict_file', methods=["POST"])
+#@swag_from('file_predict.yml')
+def predict_file():
+    """
+    Endpoint to predict stress labels from CSV file input.
+    ---
+    parameters:
+        - name: file
+          in: formData
+          type: file
+          required: true
+          description: CSV file containing texts for stress prediction with header "text"
+          example: file.csv
+    responses:
+        200:
+            description: Predicted stress labels
+            schema:
+                type: object
+                properties:
+                    label:
+                        type: integer
+                        description: Predicted stress label (0 or 1)
+                        example: 1
+    """
+    file = request.files.get('file')
+    df = pd.read_csv(file)
+    texts = df['text'].tolist()
+    labels = bb_model.predict(texts)
+    df['label'] = labels
+    return df.to_dict()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
